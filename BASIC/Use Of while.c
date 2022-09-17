b72b81339deb11c33de7ffb650ced0ed0b936a5a
AshishTodkar <ashishmali8402@gmail.com>
2022-09-17 13:17:28 +0530

    First while loop Program

---
+++ BASIC/Use Of while.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+
+    while(i<5)
+    {
+        printf("\n\t WELCOME !!!");
+        i++;
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+
+}
