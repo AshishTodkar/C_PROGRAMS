051bc25509f01c917eb9467af0bd199470a44abc
AshishTodkar <ashishmali8402@gmail.com>
2022-09-18 20:51:29 +0530

    New File Added

---
+++ BASIC/Use Of for.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+
+    for(i=1;i<=5;i++)
+    {
+        printf("\n\t HELLO !!!");
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
