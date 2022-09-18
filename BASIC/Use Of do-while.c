141aae46d2535ab5a3d2230021c5a6d69bb9cfd2
AshishTodkar <ashishmali8402@gmail.com>
2022-09-18 20:37:33 +0530

    New File Added

---
+++ BASIC/Use Of do-while.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+
+    do
+    {
+        printf("\n\t INDIA !!!");
+        i++;
+    }while(i<5);
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
