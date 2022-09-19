27ebb14c963b676f4f4d54b75ae04cc9830d7211
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-19 22:00:37 +0530

    Updated File Uploaded

---
+++ BASIC/01 Hello World.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n\t HELLO WORLD !!!");
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
