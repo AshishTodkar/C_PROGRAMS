81184769cdffeea7e463838305e8d2d3ef6ecfdf
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-24 19:34:02 +0530

    New File Added

---
+++ DIGIT/05 Even Or Odd Number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0;
+
+    printf("\n\t To Check Given Number Is Even Or Odd => \n");
+
+    printf("\n\t\t Enter a Number : ");
+    scanf("%d",&No);
+
+    if( No % 2 == 0)
+    {
+        printf("\n\t\t %d Is an Even Number.",No);
+    }
+    else
+    {
+        printf("\n\t\t %d Is an Odd Number.",No);
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
+
+
+
