20c88eee6dc6224fbb7a0d9a7bd772e6dee59d17
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-24 19:32:13 +0530

    New File Added

---
+++ DIGIT/04 Division Of Two Number.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1=0 , No2=0 , Div=0;
+
+    printf("\n\t Enter a First Number For Division : ");
+    scanf("%d",&No1);
+
+    printf("\n\t Enter a Second Number For Division : ");
+    scanf("%d",&No2);
+
+    Div = No1 / No2;
+
+    printf("\n\t Division Of %d And %d is %d.",No1,No2,Div);
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
+
+
