78bd31bd322efe2fd0e0bb676010e227b51d118d
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-24 19:40:10 +0530

    New File Added

---
+++ DIGIT/07 Positive Or Negative Number.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0;
+
+    printf("\n\t To check Given Number Is Positive Or Negative => \n");
+
+    printf("\n\t Enter A Number : ");
+    scanf("%d",&No);
+
+    if( No == 0)
+    {
+        printf("\n\t %d Is Neutral Number.",No);
+    }
+    else if( No > 0)
+    {
+        printf("\n\t %d Is Positive Number.",No);
+    }
+    else
+    {
+        printf("\n\t %d Is Negative Number.",No);
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
