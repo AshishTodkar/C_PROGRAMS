2957866809aed510efd48944633b42c0945c8162
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-25 20:08:56 +0530

    New File Added

---
+++ DIGIT/10 Swapping Of Two Number - Using Add & Sub.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1=0, No2=0;
+
+    printf("\n\t Enter a First Number For Swapping : ");
+    scanf("%d",&No1);
+
+    printf("\n\t Enter a Second Number For Swapping : ");
+    scanf("%d",&No2);
+
+    printf("\n\t Numbers Before Swapping : \n\t First Number  : %d \n\t Second Number : %d \n\t",No1,No2);
+
+    No1 = No1 + No2;
+    No2 = No1 - No2;
+    No1 = No1 - No2;
+
+    printf("\n\t Numbers After Swapping : \n\t First Number  : %d \n\t Second Number : %d \n\t",No1,No2);
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+
+}
