8c06558c3d7b2dd009ee4a8e093583dcdacffb39
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-26 20:47:46 +0530

    New File Added

---
+++ DIGIT/13 Maximum Number Out Of Two - Using Ternary.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1=0, No2=0;
+
+    printf("\n\t Enter a First Number : ");
+    scanf("%d",&No1);
+
+    printf("\n\t Enter a Second Number : ");
+    scanf("%d",&No2);
+
+    ( No1 == No2)?printf("\n\t %d And %d Are Equal Numbers.",No1,No2):( No1 > No2)?printf("\n\t %d Is Greater Than %d.",No1,No2):printf("\n\t %d Is Greater Than %d.",No2,No1);
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+
+}
