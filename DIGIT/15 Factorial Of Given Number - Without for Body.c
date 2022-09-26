3b1833d6a63287ed88e75feb5ef0fcbff8db3524
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-26 20:55:58 +0530

    New File Added

---
+++ DIGIT/15 Factorial Of Given Number - Without for Body.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0,Fact=0,Temp=0;
+
+    printf("\n\t Enter A Number To Find It's Factorial : ");
+    scanf("%d",&No);
+
+    for(Fact=1,Temp=No;Temp>0;Fact = Fact * Temp,Temp--);
+
+    printf("\n\t Factorial Of Given Number %d Is %d. ",No,Fact);
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
