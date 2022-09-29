83f78502b96ad59daf68425f1b1022295b9d3fcb
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-29 20:34:45 +0530

    New File Added

---
+++ DIGIT/25 To Print A Table Of Given Range.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int SNo=0,ENo=0,i=0,j=0;
+
+    printf("\n\t Enter A Starting Number : ");
+    scanf("%d",&SNo);
+
+    printf("\n\t Enter A Ending Number : ");
+    scanf("%d",&ENo);
+
+    if( SNo > ENo)
+    {
+        SNo = SNo + ENo;
+        ENo = SNo - ENo;
+        SNo = SNo - ENo;
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    for(i=1;i<=10;i++)
+    {
+        for(j=SNo;j<=ENo;j++)
+        {
+             printf("\t %d ",i*j);
+        }
+        printf("\n");
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
