280cc7dea8e637f80920e29c35bca3f6848764e9
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-09-29 20:32:39 +0530

    New File Added

---
+++ DIGIT/24 To Print A Table Of Given Number - In Reverse Order.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0,i=0;
+
+    printf("\n\t Enter A Number To Print It's Table : ");
+    scanf("%d",&No);
+
+    printf("\n\n===================*******======================\n\n");
+
+
+    for(i=10;i>0;i--)
+    {
+        printf("\n\t %d ",No*i);
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
