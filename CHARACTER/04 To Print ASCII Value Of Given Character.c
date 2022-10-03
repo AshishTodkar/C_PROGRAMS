22269619c469010ac7ef4d448e88f6ffc5bea321
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-03 19:26:00 +0530

    New File Added

---
+++ CHARACTER/04 To Print ASCII Value Of Given Character.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char ch='\0';
+
+    printf("\n\t Enter A Character To Print It's Corresponding ASCII Value : ");
+    ch = getche();
+
+
+    printf("\n\n\t ASCII Value Given Character %c Is %d.",ch,ch);
+
+    printf("\n\n=======================**********==============================\n\n");
+
+    getch();
+    return 0;
+}
