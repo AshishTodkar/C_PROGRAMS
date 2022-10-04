b251840f92fd23e9d18afe84e2984625085e1ec5
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-04 18:56:56 +0530

    New File Added

---
+++ PATTERN PRINTING/02 Hollow.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int r=0,c=0,Cnt=0;
+
+    printf("\n\t Enter Number Of Rows & Column Count : ");
+    scanf("%d",&Cnt);
+
+    printf("\n\t Pattern Is : \n\n\t");
+
+    for(r=1;r<=Cnt;r++)
+    {
+        for(c=1;c<=Cnt;c++)
+        {
+            if( r == 1 || r == Cnt || c == 1 || c == Cnt )
+            {
+               printf(" *");
+            }
+            else
+            {
+               printf("  ");
+            }
+        }
+        printf("\n\t");
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
