47986c952f6119e7f0899da15adfe5d9d1ff1f3b
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-09 21:39:19 +0530

    New File Added

---
+++ PATTERN PRINTING/27 LLT With Alphabets - Skips Albhabet By 2.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int r=0,c=0,Cnt=0;
+    char ch='\0';
+
+    printf("\n\t Enter Number Of Rows & Column Count : ");
+    scanf("%d",&Cnt);
+
+    printf("\n\t Pattern Is : \n\n\t");
+
+    for(ch='A',r=1;r<=Cnt;r++)
+    {
+        for(c=1;c<=Cnt;c++)
+        {
+            if( r >= c )
+            {
+               printf(" %c",ch);
+               ch+=3;
+            }
+            else
+            {
+               printf("  ");
+            }
+            if( ch > 'Z')
+            {
+                ch = ch - 26;
+            }
+        }
+        printf("\n\t");
+    }
+
+    printf("\n\n===================*******======================\n\n");
+
+    getch();
+    return 0;
+}
