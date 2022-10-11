a068f2eea0ae4963fc5bee3a33630e4d5c402457
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-11 22:36:55 +0530

    New File Added

---
+++ ARRAY/01 Array - Declaration.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[5];
+
+    printf("\n\t Value Of 1st Element = %d.",Num[0]);
+    printf("\n\t Value Of 2nd Element = %d.",Num[1]);
+    printf("\n\t Value Of 3rd Element = %d.",Num[2]);
+    printf("\n\t Value Of 4th Element = %d.",Num[3]);
+    printf("\n\t Value Of 5th Element = %d.",Num[4]);
+
+    printf("\n\n======================***********=========================\n\n");
+
+    getch();
+    return 0;
+}
