764b0d225be0a1fc2ce894909230b5a70461345c
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-12 20:21:41 +0530

    New File Added

---
+++ ARRAY/05 Operation on Elements in Array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Num[5]={10,20,30,40,50};
+
+    printf("\n\t Value Of 1st Element = %d.",Num[0]);
+    printf("\n\t Value Of 2nd Element = %d.",Num[1]);
+    printf("\n\t Value Of 3rd Element = %d.",Num[2]);
+    printf("\n\t Value Of 4th Element = %d.",Num[3]);
+    printf("\n\t Value Of 5th Element = %d.",Num[4]);
+
+    getch();
+    printf("\n\n");
+
+    Num[2]= 200;
+    Num[3]= Num[2] + Num[4];
+
+    printf("\n\t Value Of 1st Element = %d.",Num[0]);
+    printf("\n\t Value Of 2nd Element = %d.",Num[1]);
+    printf("\n\t Value Of 3rd Element = %d.",Num[2]);
+    printf("\n\t Value Of 4th Element = %d.",Num[3]);
+    printf("\n\t Value Of 5th Element = %d.",Num[4]);
+
+
+    printf("\n\n======================***********=========================\n\n");
+
+    getch();
+    return 0;
+}
+
