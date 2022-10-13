e236e985dd65f5fe487b9f5936b4626ee0ee3109
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-13 21:51:30 +0530

    New File Added

---
+++ ARRAY/10 Search A Given Element In Array By Location.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,Src=0,Num[5]={};
+
+    for(i=0;i<5;i++)
+    {
+        printf("\n\t Enter Element No. %d = ",i+1);
+        scanf("%d",&Num[i]);
+    }
+
+    printf("\n\t Enter An Element To Be Search In Above Array : ");
+    scanf("%d",&Src);
+
+    for(i=0;i<5;i++)
+    {
+        if(Num[i] == Src)
+        {
+            break;
+        }
+    }
+    if(i<5)
+    {
+        printf("\n\t An Element %d Is Found On Location %d In Above Array.",Src,i+1);
+    }
+    else
+    {
+        printf("\n\t An Element %d Is Not Found In Above Array.",Src);
+    }
+
+    printf("\n\n======================***********=========================\n\n");
+
+    getch();
+    return 0;
+}
