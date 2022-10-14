93d2e2fee8286808accbb3f3912f4716a3ac243a
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-14 22:14:45 +0530

    New File Added

---
+++ ARRAY/15 To Find Minimum Element In Array.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0,Num[5]={},Min=0;
+
+    for(i=0;i<5;i++)
+    {
+        printf("\n\t Enter Element No. %d = ",i+1);
+        scanf("%d",&Num[i]);
+    }
+
+    getch();
+
+    Min=Num[0];
+
+    for(i=0;i<5;i++)
+    {
+        if(Num[i]<Min)
+        {
+            Min=Num[i];
+        }
+    }
+
+    printf("\n\n\t Minimum Element In Above Array Is %d.",Min);
+
+    printf("\n\n======================***********=========================\n\n");
+
+    getch();
+    return 0;
+}
