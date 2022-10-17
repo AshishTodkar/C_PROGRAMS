b1dd7c5cb144cdeb127c227fbe83b2443586902f
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-17 20:09:20 +0530

    New File Added

---
+++ STRING/01 Use Of gets - puts In String.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Str[10] = {'\0'};
+
+    puts("\n\t Enter a String : ");
+    gets(Str);
+
+    puts("\n\t Given String Is : ");
+    puts(Str);
+
+    printf("\n\n========================*************============================\n\n");
+
+    getch();
+    return 0;
+
+}
