ee8cacc46ac07c0782fe0a207d2fef5577e0aeb7
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-21 21:22:31 +0530

    New File Added

---
+++ STRING/09 To Convert Given String In Togglecase.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Str[20]={'\0'};
+    int i=0;
+
+    printf("\n\t Enter A String = ");
+    gets(Str);
+
+    while(Str[i]!='\0')
+    {
+        if( Str[i]>= 'A' && Str[i]<='Z')
+        {
+            Str[i] = Str[i] + 32;
+        }
+        else if( Str[i]>= 'a' && Str[i]<='z')
+        {
+            Str[i] = Str[i] - 32;
+        }
+        i++;
+    }
+
+    printf("\n\t After Converting To Toggle case = %s.",Str);
+
+    printf("\n\n =======================**********=========================\n\n");
+
+    getch();
+    return 0;
+}
