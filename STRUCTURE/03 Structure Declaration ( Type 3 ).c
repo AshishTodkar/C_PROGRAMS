5c22ad979bd4006fe695947df70a2673e23b7fef
ASHISH TODKAR <108389221+AshishTodkar@users.noreply.github.com>
2022-10-31 21:20:41 +0530

    New File Added

---
+++ STRUCTURE/03 Structure Declaration ( Type 3 ).c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+struct stud
+{
+    int Roll_No;
+    char Name[20];
+    char City[10];
+    float Per;
+};
+
+int main()
+{
+    struct stud s1;
+
+    s1.Roll_No = 101;
+    strcpy(s1.Name,"Ash");
+    strcpy(s1.City,"Karad");
+    s1.Per = 85.00;
+
+    printf("\n\t Given Student Details : \n");
+    printf("\n\t Roll Number : %d ",s1.Roll_No);
+    printf("\n\t Name : %s ",s1.Name);
+    printf("\n\t City : %s ",s1.City);
+    printf("\n\t Percentage : %f ",s1.Per);
+
+    printf("\n\n==============================***********=================================\n\n");
+
+    getch();
+    return 0;
+}
